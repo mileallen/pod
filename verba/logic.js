ef8d96b5790535fde1c30659a02b13e4539c3132
mileallen <31962034+mileallen@users.noreply.github.com>
2020-10-12 10:48:28 +0530

    moved word options in here

--- verba/logic.js
+++ verba/logic.js
@@ -107,3 +107,33 @@ function goBack(n) {
   wrdy[`picked${n}`] = false
 }
 
+
+const wordPairs = {
+
+  level1: [
+      {word1: "dream", word2: "case"},
+      {word1: "divine", word2: "rant"},
+      {word1: "music", word2: "humble"},
+      {word1: "candor", word2: "trouble"},
+      {word1: "cognition", word2: "arbitrary"},
+      {word1: "weather", word2: "sentimental"},
+      {word1: "cant", word2: "regale"},
+      {word1: "nuance", word2: "style"},
+      {word1: "novel", word2: "bride"},
+      {word1: "weather", word2: "odd"},
+      ],
+  level2: [
+      {word1: "recalcitrant", word2: "prospective"},
+      {word1: "querulous", word2: "anterior"},
+      {word1: "inveigh", word2: "raccoon"},
+      {word1: "ken", word2: "nettle"},
+      {word1: "affect", word2: "vetting"},
+      {word1: "demur", word2: "sequestered"},
+      {word1: "fatuous", word2: "theological"},
+      {word1: "imbue", word2: "fervent"},
+      {word1: "ribald", word2: "dire"},
+      {word1: "elegiac", word2: "intensify"},
+      ]
+  }
+  
+
